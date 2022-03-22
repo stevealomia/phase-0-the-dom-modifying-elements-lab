a7ce69c80140a004de105dc7e533f0815ffc798f
Steve Alomia <stevealomia@yahoo.com>
2022-03-22 00:56:18 -0400

    commit tree 20f530c9ecf7c5a80340d52bc227be86a6c9bec0

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+//const victory = document.getElementById('victory'); 
+newHeader.textContent = 'YOUR-NAME is the champion',
+document.body.append(newHeader);
+
